b00e513151e315b85a8dd5377f7a518362949c1b
Mulvaden <105291984+Mulvaden@users.noreply.github.com>
2022-10-13 18:53:31 +0200

    Changes

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-var randomNumbers = document.getElementById("numbers").innerHTML = 
-Math.floor(Math.random() * 100);
+/* const randomNumbers = [ ];
+for (let i = 0; i < randomNumbers.length; i++) {
+    alert(i);
+    
+}
+document.getElementById("numbers").innerHTML = 
+Math.floor(Math.random() * 100); */
+
+const randomNumbers = [ ];
+for (let i = 0; i < 10; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 100));
+  }
+document.getElementById("numbers").innerHTML = randomNumbers;
+
+
+//for loop som kör tills Array är 10
+//om randomNumbers inte finns i Array
+//lägg till randomNumbers i Array med push
+
+
+const validera = () => {
+    let user = document.getElementById("user").value;
+    alert(user);
+}
